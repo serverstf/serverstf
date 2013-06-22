49c718451b65c78dad7854bfa24566d9d29c5ce6
unknown <ottajay@googlemail.com>
2013-06-22 22:20:13 +0100

    Updated templates to  make better use of template inheritence
    400.html and 500.html have not been updated

--- browser/urls.py
+++ browser/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import patterns, url
 from browser import views
 
 urlpatterns = patterns("",
-	url(r"^$", views.browse_all, name="browse.all"),
 	url(r"^(?P<region>eu|na|sa|oc|as|af)/(?P<tags>.*)$", views.browse_region, name="browse.region"),
 	url(r"^network/(?P<network_slug>[a-zA-Z0-9\-_]+)/?", views.browse_network, name="browse.network"),
 	url(r"^favourites$", views.browse_favourites, name="browse.favourites")
--- browser/views.py
+++ browser/views.py
@@ -8,28 +8,15 @@ from serverstf import iso3166
 
 import json
 
-def browse_all(request):
-	
-	template = loader.get_template("browser/browse_all.html")
-	context = RequestContext(request, {"servers": Server.objects.all()})
-	
-	return HttpResponse(template.render(context))
-
 def browse_region(request, region, tags):
 	
 	region = region.upper()
 	tags = [tag for tag in tags.split(",") if tag]
 	
-	template = loader.get_template("browser/browse_all.html")
+	template = loader.get_template("browser/region.html")
 	context = RequestContext(request, {
 							"tags": tags,
-							
-							"title": iso3166.CONTINENTS.get(region, ""),
-							
 							"region": region,
-							"batch_size": 10,
-							"allow_relist": True,
-							"initial_ids": "[]"
 							})
 	
 	return HttpResponse(template.render(context))
@@ -41,17 +28,11 @@ def browse_network(request, network_slug):
 def browse_favourites(request):
 	
 	ids = [sv.id for sv in request.user.favourites.all()];
-	
-	template = loader.get_template("browser/browse_all.html")
+
+	template = loader.get_template("browser/favourites.html")
 	context = RequestContext(request, {
 							"tags": [],
-							
-							"title": "Favourites",
-							
-							"region": "~",
-							"batch_size": 10,
-							"allow_relist": False,
-							"initial_ids": json.dumps(ids),
+							"initial_ids": ids,
 							})
 	
 	return HttpResponse(template.render(context))
