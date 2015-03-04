e55d94ca4d07618c795934893bd8df6d715ec2bf
Oliver Ainsworth <ottajay@googlemail.com>
2015-03-04 21:26:52 +0000

    Tags for TF2 game modes
    This doesn't include 5cp yet as you cannot actually infer that from
    cvars and map name.

---
+++ serverstf/tags/mode.py
@@ -0,0 +1,51 @@
+"""Tags for game modes."""
+
+
+from serverstf.tags import tag
+
+
+@tag("mode:arena", ["tf2"])
+def arena(info, players, rules, tags):
+    return "tf2" in tags and rules["rules"].get("tf_gamemode_arena") == "1"
+
+
+@tag("mode:cp", ["tf2"])
+def cp(info, players, rules, tags):
+    return "tf2" in tags and rules["rules"].get("tf_gamemode_cp") == "1"
+
+
+@tag("mode:ctf", ["tf2"])
+def ctf(info, players, rules, tags):
+    return "tf2" in tags and rules["rules"].get("tf_gamemode_ctf") == "1"
+
+
+@tag("mode:koth", ["tf2", "mode:cp"])
+def ctf(info, players, rules, tags):
+    return ("tf2" in tags
+            and "mode:cp" in tags
+            and info["map"].lower().startswith("koth_"))
+
+
+@tag("mode:mvm", ["tf2"])
+def mvm(info, players, rules, tags):
+    return "tf2" in tags and rules["rules"].get("tf_gamemode_mvm") == "1"
+
+
+@tag("mode:payload", ["tf2"])
+def payload(info, players, rules, tags):
+    return "tf2" in tags and rules["rules"].get("tf_gamemode_payload") == "1"
+
+
+@tag("mode:sd", ["tf2"])
+def sd(info, players, rules, tags):
+    return "tf2" in tags and rules["rules"].get("tf_gamemode_sd") == "1"
+
+
+@tag("mode:rd", ["tf2"])
+def rd(info, players, rules, tags):
+    return "tf2" in tags and rules["rules"].get("tf_gamemode_rd") == "1"
+
+
+@tag("mode:medival", ["tf2"])
+def medieval(info, players, rules, tags):
+    return "tf2" in tags and rules["rules"].get("tf_medieval") == "1"
