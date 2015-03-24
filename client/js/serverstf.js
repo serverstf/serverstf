19488b7d6792067b4471766c28a2e33dbf5dc72d
Oliver Ainsworth <ottajay@googlemail.com>
2015-03-24 19:38:52 +0000

    Add some over example header cover images

--- client/js/serverstf.js
+++ client/js/serverstf.js
@@ -6,3 +6,35 @@
     "ngAnimate",
     "serverstf.server"
 ]));
+
+
+(function (module) { "use strict";
+
+
+function HeaderController($scope) {
+    var self = this;
+    self.class = "foo";
+    self.query = "";
+
+    $scope.$watch(
+        function () { return self.query; },
+        function (new_) {
+            if (new_) {
+                self.class = "cover-" + new_;
+            } else {
+                self.class = "";
+            }
+        }
+    );
+}
+
+
+module.controller(
+    "HeaderController",
+    [
+        "$scope",
+        HeaderController
+    ]);
+
+
+})(angular.module("serverstf"));
