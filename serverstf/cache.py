0571b51dcb5210199ce0269e2cae51dada4fe4e2
Oliver Ainsworth <ottajay@googlemail.com>
2015-10-13 00:13:16 +0100

    Use metaclass magic to make async Cache class synchronous

--- serverstf/cache.py
+++ serverstf/cache.py
@@ -91,6 +91,8 @@ ZSET tag:<tag>
 """
 
 import asyncio
+import functools
+import inspect
 import ipaddress
 import logging
 import urllib.parse
@@ -150,7 +152,7 @@ class Address:
         return self.ip == other.ip and self.port == other.port
 
 
-class Cache:
+class AsyncCache:
     """Asynchronous access to a Redis state cache.
 
     Do not instantiate this class directly. Instead use the :meth:`connect`
@@ -171,7 +173,7 @@ class Cache:
         :param str url: the URL of the Redis database to connect to.
         :param loop: the :mod:`asyncio` event loop to use.
 
-        :return: a new :class:`Cache` instance which is bound to a Redis
+        :return: a new :class:`AsyncCache` instance which is bound to a Redis
             connection.
         """
         log.info("Connecting to cache at %s", url)
@@ -197,29 +199,83 @@ class Cache:
         """
         self._connection.close()
 
+    @asyncio.coroutine
     def get(self, address):
         log.debug("Get %s", address)
-        yield
 
 
-class SynchronousCache(Cache):
-    """A synchronous layer on top of :class:`Cache`.
+class _Synchronous(type):
+    """A metaclass for making an asynchronous API synchronous.
 
-    This effectively implements the exact same API as :class:`Cache`. Each
-    instance should be bound to an :mod:`asyncio` event loop which will be
-    used for executing the underlying asynchronous operations but will
+    A class using this metaclass must inherit from a base class which exposes
+    a number of :mod:`asyncio.coroutine` methods. The baseclass, when
+    instantiated should expose a ``loop`` attribute which is a reference to
+    the :mod:`asyncio` event loop being used by that object.
+
+    This metaclass will override the asynchronous methods so that they are
+    synchronous such that their execution will block until the underlying
+    asynchronous method has completed. The arguments the method accepts and
+    its return value will be unmodified.
+
+    The subclass may still explicitly override a method if needs be. In which
+    case the metaclass will not override it implicitly.
+
+    As this metaclass depends on the aforementioned ``loop`` attribute it is
+    only possible for this metaclass to make instance methods synchronous
+    automatically. If any asynchronous class methods are not explicitly
+    overridden then a :exc:`TypeError` will be raised.
+    """
+
+    def __new__(meta, name, bases, attrs):
+        for base in bases:
+            for attr, member in inspect.getmembers(base):
+                if asyncio.iscoroutinefunction(member):
+                    if attr not in attrs:
+                        # Check if the member is a class method
+                        if getattr(member, "__self__", None) is base:
+                            raise TypeError("The class method {!r} from {} "
+                                            "must be explicitly overriden in "
+                                            "{!r}".format(attr, base, name))
+                        attrs[attr] = meta._make_synchronous(member)
+        return super().__new__(meta, name, bases, attrs)
+
+    @staticmethod
+    def _make_synchronous(function):
+        """Make a :mod:`asyncio.coroutine` wrapped function synchronous.
+
+        Given a would-be coroutine instance method this will wrap the call in
+        ``run_until_complete`` using the event loop of the object exposed as
+        the ``loop`` attribute. All arguments and the return value are
+        propagated.
+
+        :param function: the coroutine function to wrap.
+
+        :return: a synchronous wrapper around ``function``.
+        """
+
+        @functools.wraps(function)
+        def synchronous(self, *args, **kwargs):
+            return self.loop.run_until_complete(
+                function(self, *args, **kwargs))
+
+        return synchronous
+
+
+class Cache(AsyncCache, metaclass=_Synchronous):
+    """A synchronous layer on top of :class:`AsyncCache`.
+
+    This effectively implements the exact same API as :class:`AsyncCache`.
+    Each instance should be bound to an :mod:`asyncio` event loop which will
+    be used for executing the underlying asynchronous operations but will
     block until they complete.
 
-    As with :class:`Cache` this class should not be instantiated directly.
-    Instead, :meth:`connect` must be used.
+    As with :class:`AsyncCache` this class should not be instantiated
+    directly. Instead, :meth:`connect` must be used.
     """
 
     @classmethod
     def connect(cls, url, loop):
         return loop.run_until_complete(super().connect(url, loop))
-
-    def get(self, address):
-        return self.loop.run_until_complete(super().get(address))
 
 
 def redis_url(raw_url):
@@ -259,7 +315,7 @@ def cache_main_args(parser):
 @serverstf.subcommand("cache", cache_main_args)
 def cache_main(args):
     loop = asyncio.get_event_loop()
-    cache = SynchronousCache.connect(args.url, loop)
+    cache = Cache.connect(args.url, loop)
     address = Address("94.23.226.212", 2055)
     try:
         cache.get(address)
