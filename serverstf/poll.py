d5be027246786a201bbcf10324133914e19754d1
Oliver Ainsworth <ottajay@googlemail.com>
2015-10-13 18:23:34 +0100

    Update poller module docstring to describe the design

--- serverstf/poll.py
+++ serverstf/poll.py
@@ -1,36 +1,32 @@
-"""Server state poller."""
+"""Server state poller.
+
+This module implements the ``poll`` subcommand which is a simple service
+that takes responsibility for keeping the server state cache up to date.
+
+It watches a Redis list for :class:`serverstf.cache.Address`es to poll.
+When a server is polled A2S requests are issued to it and the tags are
+re-evaluated. This updated state is then comitted to the cache.
+
+The poller has two modes: normal and passive. In the normal mode the poller
+watches the so-called 'interest queue'. The interest queue is a kind of
+priority queue where the ammount of *interest* in an address determines how
+many occurrences of it there is the queue and hence controls how frequently
+it gets polled.
+
+In passive mode the poller simply polls all servers known to the cache.
+This is done in an attempt to prevent cache states becoming too stale if
+they're not in the interest queue.
+"""
 
 import logging
 
-from valve.source import a2s
-
 import serverstf
-import serverstf.cache
-import serverstf.tags
 
 
 log = logging.getLogger(__name__)
-# cache = serverstf.cache.Cache("127.0.0.1")
-tagger = serverstf.tags.Tagger.scan(__package__ + ".tags")
-
-
-def poll(address):
-    query = a2s.ServerQuerier(address)
-    info = query.get_info()
-    players = query.get_players()
-    rules = query.get_rules()
-    tags = tagger.evaluate(info, players, rules)
-    cache.set(address, {
-        "name": info["server_name"],
-        "map": info["map"],
-        "app": info["app_id"],
-        "players": info["player_count"],
-        "bots": info["bot_count"],
-        "max": info["max_players"],
-    }, {}, tags)
 
 
 @serverstf.subcommand("poll")
-def poll_main(args):
+def _poll_main(args):
     log.info("Starting poller")
     log.info("Stopping poller")
