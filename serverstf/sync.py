132da05a5b12c7eab06c4bcc4c0df070e48ab7d8
Oliver Ainsworth <ottajay@googlemail.com>
2017-08-27 23:08:30 +0100

    Only synchronise once by default
    Adds the --forever option to the 'sync' command to get the old behaviour of
    just syncing forever back.

--- serverstf/sync.py
+++ serverstf/sync.py
@@ -5,6 +5,7 @@ master server for new servers so that they can be indexed.
 """
 
 import asyncio
+import itertools
 import logging
 
 import valve.source.a2s
@@ -26,20 +27,29 @@ log = logging.getLogger(__name__)
              "eu", "as", "oc", "af", "rest", "all"),
     help="The master server region to synchronise with.",
 )
+@serverstf.cli.argument(
+    "--forever",
+    action="store_true",
+    help="Attempt to synchronise continously."
+)
 def _sync_main(args):
     """Synchronise with the master server.
 
-    This will continually poll the master server for new server addresses.
-    These addresses are then added to the cache which is identified by the
+    This will poll the master server for new server addresses. These
+    addresses  are then added to the cache which is identified by the
     command line arguments.
 
-    Note that this merely adds the address to the cache. It doesn't poll the
-    individual servers.
+    Note that this merely adds the address to the cache. It doesn't poll
+    the individual servers.
+
+    If the ``--forever`` command line option is given, then the synchroniser
+    will run continually.
     """
     log.info("Starting master server synchroniser")
     loop = asyncio.get_event_loop()
+    running = itertools.repeat(True) if args.forever else iter([True, False])
     with serverstf.cache.Cache.connect(args.redis, loop) as cache:
-        while True:
+        while next(running):
             msq = valve.source.master_server.MasterServerQuerier()
             addresses_total = 0
             addresses_new = 0
